50d2c3b6aee90dc5633df2d3cf776322b49c7b3e
Renu shah <renu.shah@lauren.co.in>
2025-02-26 21:52:15 +0530

    changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 export default function Sidebar() {
   const [extended, setExtended] = useState(false);
+  const { onSent, prevPrompts, setRecentPrompt } = useContext(Context);
+  const loadPrompt = async (prompt) => {
+    console.log("Loading Prompt:", prompt);
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
   return (
     <div className=" flex flex-col  justify-between bg-[#f0f4f9] p-4">
       {/* Top Section */}
@@ -14,10 +21,18 @@ export default function Sidebar() {
         </div>
         <div className="mt-4">
           <p className="font-semibold">Recent</p>
-          <div className="flex items-center mt-2 gap-2 p-2 bg-gray-100 rounded-md hover:bg-[#e6eaf1] cursor-pointer">
-            <img width={20} src={assets.message_icon} alt="message" />
-            <p>What is react...</p>
-          </div>
+          {prevPrompts.map((item, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => loadPrompt(item)}
+                className="flex items-center mt-2 gap-2 p-2 bg-gray-100 rounded-md hover:bg-[#e6eaf1] cursor-pointer"
+              >
+                <img width={20} src={assets.message_icon} alt="message" />
+                <p>{item.slice(0, 18)}...</p>
+              </div>
+            );
+          })}
         </div>
       </div>
 
