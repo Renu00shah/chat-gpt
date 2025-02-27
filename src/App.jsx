1acd28d531bf387cf3ecebc12e3a075be0074382
Renu shah <renu.shah@lauren.co.in>
2025-02-27 07:39:53 +0530

    work on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,16 @@ import Main from "./components/main/Main";
 
 export default function App() {
   return (
-    <>
-      <div className="flex h-screen w-full overflow-hidden">
-        {/* Sidebar - Fixed Height */}
-        <div className="h-full w-[250px] bg-gray-200">
-          <Sidebar />
-        </div>
-
-        {/* Main - Full Width, Scrollable */}
-        <div className="flex-1 h-full overflow-y-auto">
-          <Main />
-        </div>
+    <div className="flex h-screen w-full overflow-hidden">
+      {/* Sidebar - Fixed & Responsive */}
+      <div className="h-full w-[250px] bg-gray-200 fixed left-0 top-0">
+        <Sidebar />
       </div>
-    </>
+
+      {/* Main - Scrollable Content */}
+      <div className="flex-1 h-full overflow-y-auto ml-[250px] p-4">
+        <Main />
+      </div>
+    </div>
   );
 }
